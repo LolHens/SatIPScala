168ecda5e6f6d8f4930b70c9556b7bba6da3e0c4
Pierre Kisters <pierrekisters@gmail.com>
2016-10-23 21:46:29 +0200

    Rtsp Client

--- src/main/scala/org/lolhens/satip/rtsp/RtspClient.scala
+++ src/main/scala/org/lolhens/satip/rtsp/RtspClient.scala
@@ -1,11 +1,61 @@
 package org.lolhens.satip.rtsp
 
+import java.net.Socket
+import java.util.concurrent.locks.ReentrantLock
+
+import akka.util.ByteStringBuilder
+
+import scala.concurrent.Future
+
+
 /**
   * Created by pierr on 23.10.2016.
   */
-class RtspClient(val serverHost: String,
-                 val client: Any,
-                 val cseq: Int = 1) {
+class RtspClient(val serverHost: String) {
+  private val lock = new ReentrantLock()
 
+  private val client = new Socket(serverHost, 554)
 
+  @volatile private var cseq: Int = 1
+
+  def request(request: RtspRequest): Future[RtspResponse] = Future {
+    lock.lock()
+
+    if (client.getInputStream.available() > 0)
+      throw new RuntimeException("Unexpected bytes in buffer!")
+
+    val bytes = request
+      .copy(headers =
+        request.headers + ("CSeq" -> cseq.toString))
+      .toByteString
+
+    client.getOutputStream.write(bytes.toArray)
+
+    cseq += 1
+
+    // wait for response
+    val builder = new ByteStringBuilder()
+
+    val bufferSize = client.getReceiveBufferSize
+    val buffer = new Array[Byte](bufferSize)
+
+    while ( {
+      val numBytes = client.getInputStream.read(buffer)
+      builder.putBytes(buffer, 0, numBytes)
+      client.getInputStream.available() > 0
+    }) ()
+
+    val response = RtspResponse.fromByteString(builder.result())
+
+    // TODO: maybe read full body
+
+    lock.unlock()
+    response
+  }
+
+  def close() = {
+    lock.lock()
+    client.close()
+    lock.unlock()
+  }
 }
