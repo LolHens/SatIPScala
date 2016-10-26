8c2902994fb0efa59aa3b5b146f2e09fc18551f3
LolHens <pierrekisters@gmail.com>
2016-10-26 11:04:24 +0200

    work on SatIpLocator

--- src/main/scala/org/lolhens/satip/upnp/SatIpLocator.scala
+++ src/main/scala/org/lolhens/satip/upnp/SatIpLocator.scala
@@ -4,5 +4,6 @@ package org.lolhens.satip.upnp
   * Created by pierr on 23.10.2016.
   */
 class SatIpLocator {
-
+  val searchId = "urn:ses-com:device:SatIPServer:1"
+  // TODO: Search for UPnP Device
 }
